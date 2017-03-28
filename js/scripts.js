234416e8cbf34dd15cf88c1cc4a4f6ae41681481
Trevor Fast and Tyler Stephenson <tyler@>
2017-03-28 15:18:31 -0700

    refactor example transportation site

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+var types = ["work", "fun"];
+
+var result = function() {
+  types.forEach(function(type){
+    $(`input:checkbox[name=${type}-transportation]:checked`).each(function(){
+      var TransportationMode = $(this).val();
+      $(`#${type}-responses ul`).append(`<li>${TransportationMode}</li>`);
+    });
+  });
+};
+
 $(function(){
-
-
-  
+  $("#work-responses, #fun-responses").hide();
+  $("form#transportation_survey").submit(function(event){
+    event.preventDefault();
+    result();
+    $("#work-responses, #fun-responses").show();
+    $("#transportation_survey").hide()
+  });
 });
